291565f2fbfa06e38ca6fcdc27b72a32efec92ff
dengluo <dengluo2008@163.com>
2018-04-25 14:57:49 +0800

    素材添加GitHub

---
+++ app/src/main/java/com/bauwayhome/ec/bean/BannerItem.java
@@ -0,0 +1,15 @@
+package com.bauwayhome.ec.bean;
+
+/**
+ * Created by shun8 on 2018/4/24.
+ */
+
+public class BannerItem {
+    public int image;
+    public String title;
+
+    @Override
+    public String toString() {
+        return title;
+    }
+}
